249af7652142ea85b566d5d7ce6523eed3733d74
Alex Pitenko <oleksii.p@itsyndicate.org>
2022-06-09 23:06:27 +0200

    improved DBWorker tests

--- Tests.cpp
+++ Tests.cpp
@@ -16,10 +16,42 @@ int testDBWorker() {
         cout << endl;
     }
 
-    delete worker;
+    result.clear();
+    vector<vector<string> >().swap(result);
+
+    sql = "INSERT INTO person (name, birth_date, sex) VALUES ('Test Name', '1980-05-31', 'Female');";
+    result = worker->execute(sql);
+    for (auto &row: result) {
+        for (auto &col: row) {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    cout << "inserted row" << endl;
 
     result.clear();
     vector<vector<string> >().swap(result);
+
+    string maxID = worker->execute("SELECT max(id) FROM person;")[0][0];
+
+    sql = "DELETE FROM person WHERE id = " + maxID + ";";
+    result = worker->execute(sql);
+    for (auto &row: result) {
+        for (auto &col: row) {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "deleted row" << endl;
+
+
+    result.clear();
+    vector<vector<string> >().swap(result);
+
+
+    delete worker;
+
     return 0;
 }
 
